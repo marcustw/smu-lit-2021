436de5015347cbda06269a173bcf85b065ad1eb7
Alvin <bioluminescencecloud@gmail.com>
2021-08-11 16:50:43 +0800

    Merge with marcus's

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import Background from '../components/login/Background'
-import Logo from '../components/login/Logo'
-import Header from '../components/login/Header'
-import Button from '../components/login/Button'
-import Paragraph from '../components/login/Paragraph'
-
-export default function StartScreen({ navigation }) {
-  return (
-    <Background>
-      <Logo />
-      <Header>Login Template</Header>
-      <Paragraph>
-        The easiest way to start with your amazing application.
-      </Paragraph>
-      <Button
-        mode="contained"
-        onPress={() => navigation.navigate('LoginScreen')}
-      >
-        Login
-      </Button>
-      <Button
-        mode="outlined"
-        onPress={() => navigation.navigate('RegisterScreen')}
-      >
-        Sign Up
-      </Button>
-    </Background>
-  )
-}
-=======
 import React from 'react'
 import Background from '../components/login/Background'
 import Logo from '../components/login/Logo'
@@ -52,4 +20,3 @@ export default function StartScreen({ navigation }) {
     </Background>
   )
 }
->>>>>>> 95581e506b80859bc05566545fb0267c123fced8
